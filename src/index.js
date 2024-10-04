921c533d7dd0efba0a2ac05d816c85044ad281c0
Aqil321821 <aqilrao321821@gmail.com>
2024-10-04 22:32:38 +0500

    react router 1

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.querySelector('div.rootElement'));
+root.render(<App />);
